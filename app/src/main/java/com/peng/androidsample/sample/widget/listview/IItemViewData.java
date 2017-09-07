86cc0ac790c09666a4bdd1e4d037c4ead7fb91b8
peng <419143166@qq.com>
2017-09-08 02:15:49 +0800

    [Feature] 增加列表数据模板和Log日志工具

---
+++ app/src/main/java/com/peng/androidsample/sample/widget/listview/IItemViewData.java
@@ -0,0 +1,8 @@
+package com.peng.androidsample.sample.widget.listview;
+
+/**
+ * Created by peng on 2017/9/8.
+ */
+
+public interface IItemViewData {
+}
---
+++ app/src/main/java/com/peng/androidsample/sample/widget/listview/ListViewAdapter.java
@@ -0,0 +1,59 @@
+package com.peng.androidsample.sample.widget.listview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.peng.androidsample.sample.utils.LogUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by peng on 2017/9/8.
+ */
+
+public abstract class ListViewAdapter extends BaseAdapter {
+
+    private static final String TAG = ListViewAdapter.class.getSimpleName();
+
+    private List<IItemViewData> mItemViewDataList;
+
+
+    public ListViewAdapter() {
+        super();
+        mItemViewDataList = getDataList();
+        if (mItemViewDataList == null) {
+            LogUtil.e(TAG, "DataList is null");
+            mItemViewDataList = new ArrayList<>();
+        }
+    }
+
+    public abstract List<IItemViewData> getDataList();
+
+    public abstract View getItemView(List<IItemViewData> dataList, int position, View convertView, ViewGroup parent);
+
+    @Override
+    public int getCount() {
+        return mItemViewDataList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (position >= mItemViewDataList.size()) {
+            LogUtil.e(TAG, "DataList getItem null");
+            return new Object();
+        }
+        return mItemViewDataList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getItemView(mItemViewDataList, position, convertView, parent);
+    }
+}
